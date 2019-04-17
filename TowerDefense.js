f35cc8b8e4e19c2f90c5da5e51166aa843c6ca2d
katerina <katpaltoglou@gmail.com>
2019-04-17 13:05:52 +0300

    update extracted branch

---
+++ TowerDefense.js
@@ -0,0 +1,27 @@
+
+    
+        var game = new TowerDefense($('canvas')[0])
+        
+        $('#weapons #machine-gun').click(function()
+        {
+            game.beginPlaceObject(TowerDefense.newTurret(game, 2, 5));
+        });
+    
+        $('#weapons #heavy-cannon').click(function()
+        {
+            game.beginPlaceObject(TowerDefense.newTurret(game, 2, 15));
+        });
+    
+        $('#weapons #wall').click(function()
+        {
+            game.beginPlaceObject(TowerDefense.newWall(game));
+        });
+
+        $('#weapons #explode').click(function() 
+        {
+            game.addExplosion();
+        });
+        
+        $('#pause').click(game.pause);
+        $('#resume').click(game.run);
+    
